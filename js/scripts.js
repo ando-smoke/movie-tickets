8511dd0056772c5ad6d47801acb4c35906261e03
Andy Uppendahl and Kathryn Carr <kathrynelizabethcarr@gmail.com>
2015-02-18 12:11:00 -0800

    Add functionality to Ticket.cost method to recognize a non-matinee time showing.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,14 @@ var Ticket= {
   cost: function(){
 
     var price = 10.00,
-        childDiscount = 2.00;
+        childDiscount = 2.00,
+        eveningCharge = 5.00;
 
     if (this.age <= 12) {
       price -= childDiscount;
+    }
+    if (!(this.movie.matineeTime)) {
+      price += eveningCharge;
     }
 
     return price;
--- spec/specs.js
+++ spec/specs.js
@@ -26,7 +26,18 @@ describe("Ticket", function() {
       expect(testTicket.cost()).to.equal(8.00);
     });
 
-
+    it("returns the correct cost for an adult ticket to a non-first-release " +
+      "movie showing at a non-matinee time", function() {
+      var testTicket = Object.create(Ticket);
+      var testMovie = {
+        name: "Finding Nemo",
+        firstRelease: false,
+        matineeTime: false
+      };
+      testTicket.movie = testMovie;
+      testTicket.age = 40;
+      expect(testTicket.cost()).to.equal(15.00);
+    });
   });
 
 
